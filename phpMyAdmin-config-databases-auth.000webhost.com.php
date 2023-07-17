5a7cf8db67ba3c6011424c58d76e1b57d7667434
Gabrela Guerra <123652336+GabrielaAGuerra@users.noreply.github.com>
2023-07-17 00:49:35 -0300

    Add files via upload

---
+++ phpMyAdmin-config-databases-auth.000webhost.com.php
@@ -0,0 +1,7 @@
+/* Fragmento de configuración de phpMyAdmin */
+
+/* Pegue esto en su archivo config.inc.php */
+
+$cfg['lang'] = 'es';
+$cfg['Console']['Mode'] = 'collapse';
+$cfg['ThemeDefault'] = 'bootstrap';
